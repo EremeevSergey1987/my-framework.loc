1b25098a51888458620c7cffe915f1b3c84be30b
Sergey Eremeev <eremeev87@bk.ru>
2023-04-26 13:53:04 +0800

    Роутинг 7

--- vendor/wfm/Router.php
+++ vendor/wfm/Router.php
@@ -35,15 +35,38 @@
      {
          foreach (self::$routes as $pattern => $route ){
 
-             print_r($url);
-             echo '<br/>';
-             print_r($pattern);
-             echo '<br/>';
 
              if (preg_match("#{$pattern}#", $url, $matches)) {
+                 print_r($matches);
+                 foreach ($matches as $k => $v){
+                     if (is_string($k)){
+                         $route[$k] = $v;
+                     }
+                 }
+                 if(empty($route['action'])){
+                     $route['action'] = 'index';
+                 }
+                 if(!isset($route['admin_prefix'])){
+                     $route['admin_prefix'] = '';
+                 } else {
+                     $route['admin_prefix'] = '\\';
+                 }
+                 debug($route);
+                 $route['controller'] = self::upperCamelCase($route['controller']);
+                 debug($route);
                  return true;
              }
          }
          return false;
+     }
+
+     protected static function upperCamelCase($name): string
+     {
+         return str_replace(' ', '', ucwords(str_replace('-', ' ', $name)));
+     }
+
+     protected static function loverCamelCase($name): string
+     {
+         return lcfirst(self::upperCamelCase($name));
      }
  }
